64bdd46f89bfb34e7f7e9ae8195adf1afd6359c8
Shahriaze Adnan Sany <shahriaze607@gmail.com>
2021-11-01 23:55:41 +0600

    add

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
     /*     const { user, logOut } = useFirebase(); */
     const { user, logOut } = useAuth();
     return (
-        <div className="header py-2">
+        <div className="header mx- auto py-2">
             <Container fluid>
                 <Navbar collapseOnSelect expand="lg" className="row my-2 mx-auto">
                     <div className="col-sm-12 col-md-2 mx-auto heading-img">
@@ -18,7 +18,7 @@ const Header = () => {
                     </div>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
-                        <Nav className="col-sm-12 col-md-4 d-flex justify-content-start">
+                        <Nav className="col-sm-12 col-md-4  mx-auto">
                             <Link className="fw-bold btn-style me-1" to="/Home">Home</Link>
                             <Link className="fw-bold btn-style me-1" to="/Features">Features</Link>
                             <Link className="fw-bold btn-style me-1" to="/Services">Services</Link>
@@ -28,26 +28,29 @@ const Header = () => {
 
                         <div className="col-sm-12 col-md-6 d-flex mx-auto">
                             <Form className="">
-                                {user?.email ? (
-                                    <>
-                                        <div className="div d-flex align-items-center">
-                                            <span className=" fw-bold text-dark me-2"> {user.displayName} </span>
-                                            <Nav.Link as={Link} to="/AddService" className="fw-bold btn-style me-2 text-dark">
-                                                Add Service
-                                            </Nav.Link>
-                                            <Nav.Link as={Link} to="/Myorders" className="fw-bold btn-style text-dark">
-                                                My Orders
-                                            </Nav.Link>
-                                            <Button onClick={logOut} className="fw-bold btn-style text-end mx-1">Log Out</Button>
-                                        </div>
+                                <div className="div mx-auto">
+                                    {user?.email ? (
+                                        <>
+                                            <div className="div d-flex align-items-center">
+                                                <span className=" fw-bold text-dark me-2"> {user.displayName} </span>
+                                                <Nav.Link as={Link} to="/AddService" className="fw-bold btn-style me-1 text-dark">
+                                                    Add Service
+                                                </Nav.Link>
+                                                <Nav.Link as={Link} to="/Myorders" className="fw-bold btn-style text-dark">
+                                                    My Orders
+                                                </Nav.Link>
+                                                <Button onClick={logOut} className="fw-bold btn-style text-end me-1">Log Out</Button>
+                                            </div>
 
-                                    </>
+                                        </>
 
-                                ) : (
-                                    <Nav.Link as={Link} to="/LogIn" className="fw-bold btn-style mx-2 text-dark text-end">
-                                        Log In
-                                    </Nav.Link>
-                                )}
+                                    ) : (
+                                        <Nav.Link as={Link} to="/LogIn" className="fw-bold btn-style mx-2 text-dark text-end">
+                                            Log In
+                                        </Nav.Link>
+                                    )}
+                                </div>
+
                             </Form>
                         </div>
                     </Navbar.Collapse>
